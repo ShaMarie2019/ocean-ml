3febcb8966843bf2617ceede918109f01aa5aba5
shamarie2019 <shaniquemrock@gmail.com>
2019-10-29 17:56:44 -0400

    completed challenges

--- session-10-29/session-10-29-review.py
+++ session-10-29/session-10-29-review.py
@@ -1,8 +1,13 @@
 # Welcome back!
-
+import numpy as np
 # Reviewing While loops:
 # Write code to print multiples of 5 from 45 to 90
-
+ 
+#i = 40
+#while i <= 85:
+#	i+= 5
+#	print(i)
+	
 
 # With your partner: discuss: What is a set? What is it good for?
 # Write your response as a comment here:
@@ -10,15 +15,17 @@
 
 # Create a variable called "myset" and put whatever you want in it:
 
+myset = "I love candy, it makes me happy."
 
+# Create a 2D numpy array of size 3x5 with numbers 1-15:
+are = np.array([(1,2,3) , (4,5,6), (7,8,9), (10,11,12), (13,14,15)])
 
-# Create a 2D numpy array of size 3x5 with numbers 1-5:
-
+print(are)
 
 
 # From your array, print the number 7:
 
-
+print(are[2,0])
 
 # Challenge: use while loop to print the following pattern:
 # ****
