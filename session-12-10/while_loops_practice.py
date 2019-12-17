84bea379bfc1a59f1a7a1d9152f6a2fe166faf74
shamarie2019 <shaniquemrock@gmail.com>
2019-12-17 17:00:58 -0500

    12/17/19

--- session-12-10/while_loops_practice.py
+++ session-12-10/while_loops_practice.py
@@ -5,3 +5,24 @@
 
 # 3. Use a while loop to find the average of these numbers:
 numbers = [10,42,-2, 900,5,8,39]
+
+i = 0
+while i <= 9:
+	i+= 1
+	print(i)
+
+a = 0
+while a <= 216:
+	a += 24
+	print(a)
+
+total = 0
+l = len(numbers)
+while i > l:
+	total += numbers[i]
+	i+=1
+
+print(v)
+avg = v/l
+print(avg)
+
