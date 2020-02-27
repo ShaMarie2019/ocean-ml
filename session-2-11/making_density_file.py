b961fbd00687bacee1a3968d0bb0226a74c82639
shamarie2019 <shaniquemrock@gmail.com>
2020-02-27 17:07:24 -0500

    2/27/20

--- session-2-11/making_density_file.py
+++ session-2-11/making_density_file.py
@@ -1,5 +1,6 @@
 from netCDF4 import Dataset
 import numpy as np
+import seawater as sw 
 
 dataset = Dataset(r'/Users/brownscholar/Desktop/Intern_Git/Downloads/dataset-armor-3d-rep-weekly_1574699840388.nc')
 
@@ -9,18 +10,31 @@ pressure = dataset ['depth']
 temperture = dataset['to']
 salinity = dataset['so']
 
+
+
 print(pressure.shape)
 print(temperture.shape)
 print(salinity.shape)
 
 #second step: make the pressure data 3 dimensional 
 
-pressure_3d = np.zeros((31,80,27))
+pressure_3d = np.zeros((31,80,27)) # this is a empty array that is created using the lon and lat 
 
-for i in pressure:
-	pressure_3d[i,:,:]
+# lvl_1 = np.repeat(10,80*27)
 
+# lvl_2 = np.repeat(20,80*27)
+
+for depth_level in pressure:
+	print(np.repeat(depth_level,80*27).reshape((80,27)))
 	
+
+for i in range(0,31):
+	#print(np.repeat(pressure[i],80*27).reshape((80,27)))
+	pressure_3d[i,:,:] = np.repeat(pressure[i],80*27).reshape((80,27))
+	print(pressure_3d[i,:,:])
+
+density = sw.dens(salinity[:],temperture[:],pressure_3d)
+print(density)
 # to do: 
 
 #imputs: salinity, tempature, pressure in form of netcdf file 
