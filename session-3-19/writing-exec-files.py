3b64a580202e7de2273b1e5a3bb963ef90a3f83c
shamarie2019 <shaniquemrock@gmail.com>
2020-03-24 16:40:24 -0400

    3/24/2020

--- session-3-19/writing-exec-files.py
+++ session-3-19/writing-exec-files.py
@@ -1,1 +1,14 @@
 # to do
+
+from netCDF4 import Dataset
+import datetime as td
+
+
+dataset = Dataset(r'/Users/brownscholar/Desktop/Intern_Git/Downloads/dataset-armor-3d-rep-weekly_1574699840388.nc')
+
+print(vectorq.exec)
+print(omegainv.exec)
+
+time = dataset["time"][:]
+Set_Time_Variable = td.date(1993,1,6)
+
